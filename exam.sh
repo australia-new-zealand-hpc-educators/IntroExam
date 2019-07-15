598a5859b9793a115e92e1465a95d52725f9d49c
Lev Lafayette <lev@levlafayette.com>
2019-07-16 09:34:45 +1000

    Added introduction function

--- exam.sh
+++ exam.sh
@@ -3,9 +3,27 @@
 # This is an summative exam for Introduction to Linux and High Performance 
 # Computing.
 #
-# Lev Lafayette, 20190715
+# Lev Lafayette, 2019
+
+introduction() {
+	clear
+	cat << EOF
+-------------------------------------------------------------------------
+Welcome to the Introduction to Linux and HPC Exam!
+
+This exam will take 60 minutes to complete and includes 60 questions from
+the introductory course with 15 questions in each subsection.
+
+Questions may have more than one way of getting the correct answer.
+
+Your time will start when a key is entered.
+--------------------------------------------------------------------------
+EOF
+	read -n1 -r -p "Press any key to begin"
+}
 
 questions() {
+	clear
 	shuf -n 3 dataset.md -o questions.md 
 	input="questions.md"
 	while IFS= read -r line
@@ -16,9 +34,11 @@ questions() {
 
 
 main() {
+	introduction
 	SECONDS=0
 	questions
-	read -n1 -r -p "Press any key to continue"
+	read -n1 -r -p "Press any key to finish"
+	clear
 	duration=$SECONDS
 	echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed."
 }
